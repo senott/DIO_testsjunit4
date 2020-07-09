89299d2e82a8b7e8e22024905e123d70b8011577
Senott <senott@yahoo.com>
2020-07-09 15:06:26 -0300

    Initial commit

---
+++ src/test/java/com/test/CalculadoraTest.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CalculadoraTest {
+
+    @Test
+    public void testSomar() {
+
+        Calculadora calculadora = new Calculadora();
+
+        int soma = calculadora.somar("1+5+6");
+
+        assertEquals(12, soma);
+
+    }
+
+    @Test
+    public void testSomarComMock() {
+        Calculadora calculadora = mock(Calculadora.class);
+        when(calculadora.somar("1+2")).thenReturn(10);
+        int resultado = calculadora.somar("1+2");
+        assertEquals(10, resultado);
+    }
+}
